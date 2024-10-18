76492e5feafbc43dbdcf00447518252ed5e936a5
魔法星辰 <141453555+Mofa-Xingche@users.noreply.github.com>
2024-10-19 04:00:19 +0900

    Update basic.py
    tsukuyomiのとき、なぜかこうしないと動かなかったですがどうなんでしょうか

--- sbv2_bindings/examples/basic.py
+++ sbv2_bindings/examples/basic.py
@@ -3,16 +3,15 @@ from sbv2_bindings import TTSModel
 
 def main():
     print("Loading models...")
-    model = TTSModel.from_path("../models/debert.onnx", "../models/tokenizer.json")
+    model = TTSModel.from_path("../models/deberta.onnx", "../models/tokenizer.json")
     print("Models loaded!")
 
     model.load_sbv2file_from_path("amitaro", "../models/amitaro.sbv2")
     print("All setup is done!")
 
-    style_vector = model.get_style_vector("amitaro", 0, 1.0)
     with open("output.wav", "wb") as f:
         f.write(
-            model.synthesize("おはようございます。", "amitaro", style_vector, 0.0, 0.5)
+            model.synthesize("おはようございます。", "amitaro", 0, 0.0, 0.5)
         )
 
 
